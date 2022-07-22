fccc8ba1b9e2236e1aa5faef0bdb9568057d5494
Harsh Pandit <harsh.pandit2004@gmail.com>
2022-07-22 08:48:50 +0530

    the dom is not getting updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,31 @@
 import Navbar from "./Components/Navbar";
+import {BrowserRouter, Route} from 'react-router-dom';
+import "./App.css";
 
-import './App.css';
+//importing the screens:
+import Home from "./Components/Screens/Hoe";
+import Login from "./Components/Screens/Login";
+import Profile from "./Components/Screens/Profile";
+import Signup from "./Components/Screens/Signup";
+
 
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Route exact path = "/">
+        <Home/>
+      </Route>
+      <Route path = "/login">
+        <Login/>
+      </Route>
+      <Route path = "/Profile">
+        <Profile/>
+      </Route>
+      <Route path = "/signup">
+        <Signup />
+      </Route>
+    </BrowserRouter>
   );
 }
 
--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -1,74 +1,26 @@
 import React from "react";
+import {Link} from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Navbar.css";
 const Navbar = () => {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg bg-light">
-        <div className="container-fluid">
-          <p>Navbar</p>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNavDropdown"
-            aria-controls="navbarNavDropdown"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Features
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Pricing
-                </a>
-              </li>
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="#"
-                  id="navbarDropdownMenuLink"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Dropdown link
-                </a>
-                <ul
-                  className="dropdown-menu"
-                  aria-labelledby="navbarDropdownMenuLink"
-                >
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Action
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Another action
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Something else here
-                    </a>
-                  </li>
-                </ul>
-              </li>
-            </ul>
-          </div>
+      <nav>
+        <div className="nav-wrapper white">
+          <Link to="/" className="brand-logo">
+            Logo
+          </Link>
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <li>
+              <Link to="/login">Log-in</Link>
+            </li>
+            <li>
+              <Link to="/signup">Sign-up</Link>
+            </li>
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+          </ul>
         </div>
       </nav>
     </div>
