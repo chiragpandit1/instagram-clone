122f5fc2fbc7ded7c9b4163735494083fd085df7
Harsh Pandit <harsh.pandit2004@gmail.com>
2022-07-22 13:58:31 +0530

    created the profile page

---
+++ frontend/src/Components/Screens/sub-components/Accountinfo.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "../css/accountinfo.css";
+const Accountinfo = () => {
+  return (
+    <div>
+      <div className="primarydiv">
+        <div className="profile-picture-waladiv">
+          <img
+            src="https://lh5.googleusercontent.com/maQ_zzdRLSRwpAVoXHgJHjxIKu3LbbEeIAkUuoeo1iGJm-6zeI2vf2Lfl3m2fX4bW9rSI5gyCJFY1Tv0mZrn=w1440-h764-rw"
+            alt="profilepicture"
+          />
+        </div>
+        <div className="Account-data-waladiv">
+          <div className="account-name">
+            <div className="sub-account-name">
+              definitely-not-kermit-the-god-420
+            </div>
+            <button className="sub-account-name-button">Edit Profile</button>
+            <div className="material-symbols-outlined">settings
+            </div>
+          </div>
+          <div className="account-details">
+            <div className="sub-account-details">
+              0 posts (cause he's a chad)
+            </div>
+            <div className="sub-account-details">1k followers (he's cool)</div>
+            <div className="sub-account-details">
+              1 following (typical chad)
+            </div>
+          </div>
+          <div className="account-discription">
+            Wassup Internet, i am not-kermit!
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Accountinfo;
---
+++ frontend/src/Components/Screens/sub-components/Posts.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "../css/posts.css";
+const Posts = () => {
+  return (
+    <div className="posts">
+      <img
+        className="Post"
+        src="https://lh5.googleusercontent.com/maQ_zzdRLSRwpAVoXHgJHjxIKu3LbbEeIAkUuoeo1iGJm-6zeI2vf2Lfl3m2fX4bW9rSI5gyCJFY1Tv0mZrn=w1440-h764-rw"
+        alt="post"
+      />
+      <img
+        className="Post"
+        src="https://lh5.googleusercontent.com/maQ_zzdRLSRwpAVoXHgJHjxIKu3LbbEeIAkUuoeo1iGJm-6zeI2vf2Lfl3m2fX4bW9rSI5gyCJFY1Tv0mZrn=w1440-h764-rw"
+        alt="post"
+      />
+      <img
+        className="Post"
+        src="https://lh5.googleusercontent.com/maQ_zzdRLSRwpAVoXHgJHjxIKu3LbbEeIAkUuoeo1iGJm-6zeI2vf2Lfl3m2fX4bW9rSI5gyCJFY1Tv0mZrn=w1440-h764-rw"
+        alt="post"
+      />
+      <img
+        className="Post"
+        src="https://lh5.googleusercontent.com/maQ_zzdRLSRwpAVoXHgJHjxIKu3LbbEeIAkUuoeo1iGJm-6zeI2vf2Lfl3m2fX4bW9rSI5gyCJFY1Tv0mZrn=w1440-h764-rw"
+        alt="post"
+      />
+      <img
+        className="Post"
+        src="https://lh5.googleusercontent.com/maQ_zzdRLSRwpAVoXHgJHjxIKu3LbbEeIAkUuoeo1iGJm-6zeI2vf2Lfl3m2fX4bW9rSI5gyCJFY1Tv0mZrn=w1440-h764-rw"
+        alt="post"
+      />
+      <img
+        className="Post"
+        src="https://lh5.googleusercontent.com/maQ_zzdRLSRwpAVoXHgJHjxIKu3LbbEeIAkUuoeo1iGJm-6zeI2vf2Lfl3m2fX4bW9rSI5gyCJFY1Tv0mZrn=w1440-h764-rw"
+        alt="post"
+      />
+    </div>
+  );
+};
+
+export default Posts;
